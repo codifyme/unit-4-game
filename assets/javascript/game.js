860a51d73e2ab02483836a701c76929c24db1cc4
Angel Cochachez <acochachez@gmail.com>
2019-07-22 15:33:35 -0700

    added an updates on my stylesheet and Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
+// random Result
 var random_result;
-var lost = 0;
-var win = 0;
+
+//Game counter
+var lostCounter = 0;
+var winCounter = 0;
+
+//Previous Number
 var previous = 0;
 
 //$(".crystal").attr('class');
@@ -63,7 +68,13 @@ $(document).on('click', '.crystal', function() {
     resetAndStart();
   }
 });
-//PSeudocode
+// Speudo Coding
+// I have a game of 4 crystals and random Result
+// Every crystal have a random number between 1-12 as it mentioned in the assignment
+// A new random number should be generate every single time a LOST or WIN on those 4 crystals
+// By ckicling in any crystal, it should be adding in the previous result until it equal to the random result
+// If it is greater than the random result, it decrement a lost counter
+//if it is equal, then we increment a win counter
 //It look like this program is showing the Total score but not
 //the win and lost. I tried to find out what is causing problem regarding the first "resetAndStart" as well as the second and third "resetAnd Start"
 //Hopefully is not a big problem but I tried to find out with all the review of inside class activities.
